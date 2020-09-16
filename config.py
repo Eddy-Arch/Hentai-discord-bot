ea2847cb00fc7c752921247360fb3afc901f605d
Eddy <bishclapper69420@gmail.com>
2020-09-16 23:59:55 +0300

    improved help command, cleaned code up

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 token = ""
-#the id of the chat to send bot logs of admin value i.e (kick, ban, purge, etc)
+# the id of the chat to send bot logs of admin value i.e
+# (kick, ban, purge, etc)
 admin_actions_log_channel_id = 741923499542839330
-#the id of the chat to send bot logs of like general commands that were executed
+# the id of the chat to send bot logs of like general commands that were
+# executed
 general_actions_log_channel_id = 741923499542839330
-#the name of the role the user should get once theyve been verified
+# the name of the role the user should get once theyve been verified
 verify_role_name = 'Verified'
-#the prefix to be used
+# the prefix to be used
 conf_bot_prefix = "+"
--- index.py
+++ index.py
@@ -12,125 +12,211 @@ from discord.ext.commands import has_permissions, CheckFailure
 import sys
 import os
 import config
-from config import token, admin_actions_log_channel_id, general_actions_log_channel_id, verify_role_name, conf_bot_prefix
+from config import token, admin_actions_log_channel_id
+from config import general_actions_log_channel_id
+from config import verify_role_name, conf_bot_prefix
 from datetime import datetime
-
-##commited from arch linux belive it or not i know this is mega swagg innit
 from colorama import init
 
 init()
-#set the preifx and disable the builtin help command that comes with discord.py
+# set the preifx and disable the builtin help command that comes with
+# discord.py
+
 client = commands.Bot(command_prefix=conf_bot_prefix)
 client.remove_command("help")
 
-#all of the available commands sent as a message. it consists of about 5 embedded messages with different colors. i recommend deleting this, and just linking to a website for help.
+# all of the available commands sent as a message. it consists of about 5 
+# embedded messages with different colors. i recommend deleting this, 
+# and just linking to a website for help.
 @client.command()
 async def help(ctx):
     author = ctx.message.author
 
-    embed = discord.Embed(
-        colour=discord.Color.purple()
-    )
 
-    embed2 = discord.Embed(
+    helpembed= discord.Embed(
+        colour=discord.Color.green()
+    )
+    helpembed.set_author(name="Options")
+    helpembed.add_field(name="+help_nsfw", value = "available nsfw commands")
+    helpembed.add_field(name="+help_more_nsfw", value = "more available nsfw \
+                        commands")
+    helpembed.add_field(name="+help_social", value = "available social comm\
+                        ands")
+    helpembed.add_field(name="+help_admin", value = "available admin \
+                        commands")
+    helpembed.add_field(name="+help_misc", value = "available misc \
+                        commands")
+
+
+    await ctx.author.send(embed=helpembed)
+
+
+###help commands
+@client.command()
+async def help_nsfw(ctx):
+    author = ctx.message.author
+    embed = discord.Embed(
         colour=discord.Color.blurple()
     )
 
-    embed3 = discord.Embed(
-        colour=discord.Color.magenta()
-    )
 
-    embed4 = discord.Embed(
-        colour=discord.Color.blue()
-    )
-    embed5 = discord.Embed(
-        colour=discord.Color.red()
-    )
 
-    embed5.set_author(name="Misc")
     embed.set_author(name="Available NSFW commands")
     embed.add_field(name="+feet", value='NSFW feet pics', inline=False)
     embed.add_field(name="+yuri", value='NSFW yuri pics', inline=False)
     embed.add_field(name="+trap", value='NSFW trap pics', inline=False)
-    embed.add_field(name="+futanari", value='NSFW futanari pics', inline=False)
-    embed.add_field(name="+hololewd", value='NSFW hololewd pics', inline=False)
-    embed.add_field(name="+lewdkemo", value='NSFW lewdkemo pics', inline=False)
-    embed.add_field(name="+solo_gif", value='NSFW solo gifs', inline=False)
-    embed.add_field(name="+feet_gif", value='NSFW feet gif', inline=False)
-    embed.add_field(name="+cum", value='NSFW cum on catgirls pics', inline=False)
+    embed.add_field(name="+futanari",
+                    value='NSFW futanari pics', inline=False)
+    embed.add_field(name="+hololewd",
+                    value='NSFW hololewd pics', inline=False)
+    embed.add_field(name="+lewdkemo",
+                    value='NSFW lewdkemo pics', inline=False)
+    embed.add_field(name="+solo_gif",
+                    value='NSFW solo gifs', inline=False)
+    embed.add_field(name="+feet_gif",
+                    value='NSFW feet gif', inline=False)
+    embed.add_field(name="+cum",
+                    value='NSFW cum on catgirls pics', inline=False)
     embed.add_field(name="+erokemo", value='NSFW erokemo pics', inline=False)
     embed.add_field(name="+les", value='NSFW les pics', inline=False)
     embed.add_field(name="+wallpaper", value='cute wallpapers', inline=False)
     embed.add_field(name="+lewdk", value='NSFW lewdk pics', inline=False)
-    embed.add_field(name="+neko_gif", value='cute neko pics :flushed:', inline=False)
+    embed.add_field(name="+neko_gif",
+                    value='cute neko pics :flushed:', inline=False)
     embed.add_field(name="+meow", value='cute cat pics', inline=False)
-    embed3.add_field(name="+tickle", value='usage: +tickle @user tickle tickle', inline=False)
-    embed.add_field(name="+lewd", value='lewd catgirls', inline=False)
-    embed3.add_field(name="+feed", value='usage: +feed @user eat up fatty', inline=False)
-    embed.add_field(name="+gegc", value='genetically engineerd catgirl memes', inline=False)
-    embed.add_field(name="+eroyuri", value='NSFW eroyuri', inline=False)
-    embed.add_field(name="+eron", value='NSFW eron', inline=False)
-    embed.add_field(name="+bj", value='NSFW bj', inline=False)
-    embed.add_field(name="+nsfw_neko_gif", value='NSFW neko gif', inline=False)
-    embed.add_field(name="+solo", value='NSFW solo pic', inline=False)
-    embed.add_field(name="+kemonomimi", value='NSFW kemonomimi pic', inline=False)
-    embed2.add_field(name="+nsfw_avatar", value='NSFW avatar pic for u horny virgins', inline=False)
-    embed3.add_field(name="+gasm", value='usage: +gasm @user no way dude', inline=False)
-    embed3.add_field(name="+poke", value='usage: +poke @user beep', inline=False)
-    embed3.add_field(name="+slap", value='usage: +slap @user u cunt', inline=False)
+    embed.add_field(name="+lewd",
+                    value='lewd catgirls', inline=False)
+    embed.add_field(name="+gegc",
+                    value='genetically engineerd catgirl memes', inline=False)
+    embed.add_field(name="+eroyuri",
+                    value='NSFW eroyuri', inline=False)
+    embed.add_field(name="+eron",
+                    value='NSFW eron', inline=False)
+    embed.add_field(name="+bj",
+                    value='NSFW bj', inline=False)
+    embed.add_field(name="+nsfw_neko_gif",
+                    value='NSFW neko gif', inline=False)
+    embed.add_field(name="+solo",
+                    value='NSFW solo pic', inline=False)
+    embed.add_field(name="+kemonomimi",
+                    value="kemonomimi", inline =False)
+    await ctx.author.send(embed=embed)
+@client.command()
+async def help_more_nsfw(ctx):
+    author = ctx.message.author
+    embed2 = discord.Embed(
+        colour=discord.Color.blurple()
+    )
+    embed2.set_author(name="More NSFW")
+    embed2.add_field(name="+nsfw_avatar",
+                     value='NSFW avatar pic for u horny virgins',
+                     inline=False)
     embed2.add_field(name="+anal", value='NSFW anal pic', inline=False)
     embed2.add_field(name="+hentai", value='NSFW hentai pic', inline=False)
-    embed2.add_field(name="+avatar", value='generates a dope avatar pic', inline=False)
+    embed2.add_field(name="+avatar",
+                     value='generates a dope avatar pic', inline=False)
     embed2.add_field(name="+erofeet", value='NSFW erofeet', inline=False)
     embed2.add_field(name="+pussy", value='NSFW pussy', inline=False)
     embed2.add_field(name="+tits", value='NSFW tits', inline=False)
-    embed2.add_field(name="+waifu", value='waifu. self explanotory you weeb', inline=False)
+    embed2.add_field(name="+waifu",
+                     value='waifu. self explanotory you weeb', inline=False)
     embed2.add_field(name="+boobs", value='boobs', inline=False)
     embed2.add_field(name="+smallboobs", value='smallboobies ', inline=False)
-    embed3.add_field(name="+pat", value='usage: +pet @user u cutie', inline=False)
-    embed3.add_field(name="+kiss", value='usage: +kiss @user muah (no homo)', inline=False)
-    embed3.add_field(name="+spank", value='usage: +spank @user BRUH', inline=False)
-    embed3.add_field(name="+cuddle", value='usage: +cuddle @user u cutie', inline=False)
-    embed3.add_field(name="+hug", value='usage: +hug @user u cutie', inline=False)
     embed2.add_field(name="+fox_girl", value='fox girl pics', inline=False)
     embed2.add_field(name="+cat", value='cute kitty pics', inline=False)
     embed2.add_field(name="+neko", value='neko pics', inline=False)
-    embed3.add_field(name="+owoify", value='usage: "owoifys" some text', inline=False)
-    embed4.set_author(name="Moderation")
+    await ctx.author.send(embed=embed2)
+
+@client.command()
+async def help_social(ctx):
+    author = ctx.message.author
+
+    embed3 = discord.Embed(
+        colour=discord.Color.magenta()
+    )
     embed3.set_author(name="Social")
-    embed2.set_author(name="More NSFW")
+
+    embed3.add_field(name="+tickle",
+                     value='usage: +tickle @user tickle tickle', inline=False)
+    embed3.add_field(name="+feed",
+                     value='usage: +feed @user eat up fatty', inline=False)
+    embed3.add_field(name="+gasm",
+                     value='usage: +gasm @user no way dude', inline=False)
+    embed3.add_field(name="+poke",
+                     value='usage: +poke @user beep', inline=False)
+    embed3.add_field(name="+slap",
+                     value='usage: +slap @user u cunt', inline=False)
+    embed3.add_field(name="+pat",
+                     value='usage: +pet @user u cutie', inline=False)
+    embed3.add_field(name="+kiss",
+                     value='usage: +kiss @user muah (no homo)', inline=False)
+    embed3.add_field(name="+spank",
+                     value='usage: +spank @user BRUH', inline=False)
+    embed3.add_field(name="+cuddle",
+                     value='usage: +cuddle @user u cutie', inline=False)
+    embed3.add_field(name="+hug",
+                     value='usage: +hug @user u cutie', inline=False)
+    embed3.add_field(name="+owoify",
+                     value='usage: "owoifys" some text', inline=False)
+    await ctx.author.send(embed=embed3)
+
+@client.command()
+async def help_admin(ctx):
+    author = ctx.message.author
+
+    embed4 = discord.Embed(
+        colour=discord.Color.blue()
+    )
+    embed4.set_author(name="Moderation")
 
     embed4.add_field(name="+timeban",
-                     value='usage: +timeban @user [reason] [time, in seconds] (this will send them a dm notifying them that theyve been temporarily banned for a reason u specify)',
+                     value='usage: +timeban @user [reason] [time, in seconds]\
+                      (this will send them a dm notifying them that theyve \
+                     been temporarily banned for a reason u specify)',
                      inline=False)
     embed4.add_field(name="+kick",
-                     value='usage: +kick @user [reason] (this will send them a dm notifying them that theyve been kicked for a reason u specify)',
+                     value='usage: +kick @user [reason] (this will send them\
+                     a dm notifying them that theyve been kicked for a reason\
+                     u specify)',
                      inline=False)
     embed4.add_field(name="+ban",
-                     value='usage: +ban @user [reason] (this will send them a dm notifying them that theyve been banned for a reason u specify)',
+                     value='usage: +ban @user [reason] (this will send them\
+                     a dm notifying them that theyve been banned for a \
+                     reason u specify)',
                      inline=False)
-    embed4.add_field(name="+purge", value='usage: +purge <amount of messages to purge>', inline=False)
+    embed4.add_field(name="+purge", value='usage: +purge <amount of messages \
+                     to purge>', inline=False)
     embed4.add_field(name="+warn",
-                     value='usage: +warn @user [reason] (this will send them a dm notifying them that theyve been warned for a reason u specify)',
+                     value='usage: +warn @user [reason] (this will send them \
+                     a dm notifying them that theyve been warned for a  \
+                     reason u specify)',
                      inline=False)
     embed4.add_field(name="+contribute",
-                     value='get the github repository link, to which you can contribute if you choose to do so. (please do)',
+                     value='get the github repository link, to which you can \
+                     contribute if you choose to do so. (please do)',
                      inline=False)
-    embed5.add_field(name="+wordsfromgod", value='gives you a list of random words, which are the words of god. inspired by terry a davis, RIP.')
-    embed5.add_field(name="+coronavirus", value = 'usage: !coronavirus <country>. gives you the current world stats of the pandemic')
-
-    await ctx.author.send(embed=embed)
-    await ctx.author.send(embed=embed2)
-    await ctx.author.send(embed=embed3)
     await ctx.author.send(embed=embed4)
+
+@client.command()
+async def help_misc(ctx):
+    author = ctx.message.author
+
+    embed5 = discord.Embed(
+        colour=discord.Color.red()
+    )
+    embed5.set_author(name="Misc")
+    embed5.add_field(name="+wordsfromgod", value='gives you a list of random \
+                     words, which are the words of god. inspired by terry \
+                     a davis, RIP.')
+    embed5.add_field(name="+coronavirus", value = 'usage: !coronavirus \
+                     <country>. gives you the current world stats o\
+                     f the pandemic')
     await ctx.author.send(embed=embed5)
 
 
-# fox_girl
-
-
 print(
-    Fore.WHITE + "[" + Fore.BLUE + '+' + Fore.WHITE + "]" + Fore.BLUE + " attempting to establish connection to the client")
+    Fore.WHITE + "[" + Fore.BLUE + '+' + Fore.WHITE + "]" + Fore.BLUE + " att\
+    empting to establish connection to the client")
 
 
 @client.event
@@ -145,7 +231,6 @@ async def verify(ctx, * role: discord.Role):
   await user.add_roles(role)
   await ctx.send("you've been verified ")
 
-    
 @client.command()
 async def info(ctx):
     author = ctx.message.author
@@ -155,9 +240,12 @@ async def info(ctx):
     )
     embed.set_author(name="Info")
     embed.add_field(name="Creator:", value='Eddy-Gentoo#1169', inline=False)
-    embed.add_field(name="Source code", value='https://github.com/Eddy-Arch/NSFW_Discordb_bot/', inline=False)
-    embed.add_field(name="Contact", value='you can contact me via discord or email', inline=False)
-    embed.add_field(name="Personal Website", value='https://eddster.xyz', inline=False)
+    embed.add_field(name="Source code", value='https://github.com/Eddy-Arch/N\
+                    SFW_Discordb_bot/', inline=False)
+    embed.add_field(name="Contact", value='you can contact me via discord or \
+                    email', inline=False)
+    embed.add_field(name="Personal Website", value='https://eddster.xyz',
+                    inline=False)
     await ctx.send(embed=embed)
 
 
@@ -378,15 +466,14 @@ async def timeban(ctx, member: discord.Member, time = None, *, reason=None, ):
             #    await asyncio.sleep(int(time))
             #    print(f"{author} just unbanned {member} for {reason}")
             #    await ctx.guild.unban(member)
-            else:
                 #embed = discord.Embed(title="Permission Denied.",
                 #                      description="You don't have permission to use this command.",
                 #                      color=discord.Color.red())
                 #await ctx.send(embed=embed)
         except:
             embed = discord.Embed(title="Permission Denied.",
-                                  description="Bot doesn't have correct permissions, or bot can't ban this user.",
-                                  color=discord.Color.red())
+                                description="Bot doesn't have correct permissions, or bot can't ban this user.",
+                                color=discord.Color.red())
 #            if "m" in time:
 #                print("inital time: " + time)
 #               bruhstr = time.replace("m", "")
